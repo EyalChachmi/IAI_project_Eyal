46eedc047ad2e013ca99b7b03b9d15b692facccc
eyal chachmishvily <eyalchachmi@gmail.com>
2025-11-02 10:50:46 +0200

    updated vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,7 +43,7 @@ variable "min_nodes" {
 variable "max_nodes" {
   description = "Maximum number of worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "kubernetes_version" {
